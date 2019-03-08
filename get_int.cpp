20415ca662d89fc01ad1564c5891e600d7f15323
Martin Tonhauzer <unknow112@gmail.com>
2019-03-08 23:34:48 +0100

    pridane vyberanie rozhrani cez konfig subor

--- get_int.cpp
+++ get_int.cpp
@@ -5,18 +5,44 @@
 #include <tins/network_interface.h>
 #include <algorithm>
 #include <vector>
+#include <unordered_set>
 #include <fstream>
+
+
+#include <iostream>
+
+auto desired_interfaces()
+{
+    std::ifstream i("selected_interfaces.txt");
+    if (!i){
+        throw std::domain_error("Missing file 'selected_interfaces.txt' necessary configuration! ");
+    }
+    std::unordered_set<std::string> result;
+    std::string line ;
+    std::getline(i, line);
+    while (!i.eof()){
+        result.insert(line);
+        std::getline(i, line);
+    }
+
+    return result;
+}
+
 
 
 std::vector<Tins::NetworkInterface> get_interfaces(){
     auto interfaces = Tins::NetworkInterface::all();
+    auto desired = desired_interfaces();
     interfaces.erase(
         std::remove_if(
             interfaces.begin(),
             interfaces.end(),
-            [](const Tins::NetworkInterface& s){ return s.name() != "enp0s25";}
+            [&desired](const Tins::NetworkInterface& s){ return desired.find(s.name()) == desired.end();}
         ),
         interfaces.end()
     );
+    if (interfaces.size() != 2){
+        throw std::domain_error("Wrong interface config, didnt got desired interface count!");
+    }
     return interfaces;
 }
